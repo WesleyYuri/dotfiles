f7a3b241dc9d564ef070d644e6bd8b6a4f5b3522
Wesley Yuri <wesleyyuridev@gmail.com>
2024-11-03 16:16:39 -0300

    fix config.fish: switch if to inline if

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,29 +6,12 @@ if status is-interactive
 	. ~/.asdf/plugins/java/set-java-home.fish
 	source ~/.asdf/plugins/golang/set-env.fish
 
-	if [ -d $HOME/software-development ]
-		alias dev="cd $HOME/software-development"
-	end
-
-	if [ -e /bin/docker ]
-		alias docker-compose="docker compose"
-	end
-
-	if [ -e /bin/lsd ]
-		alias ls="lsd"
-	end
-
-	if [ -e /bin/nvim ]
-		alias vim="nvim"
-	end
-
-	if [ -e /bin/lazygit ]
-		alias lg="lazygit"
-	end
-
-	if [ -z "$TMUX" ]
-		tmux
-	end
+	if [ -d $HOME/software-development ]; alias dev="cd $HOME/software-development"; end
+	if [ -e /bin/docker ]; alias docker-compose="docker compose"; end
+	if [ -e /bin/lsd ]; alias ls="lsd"; end
+	if [ -e /bin/nvim ]; alias vim="nvim"; end
+	if [ -e /bin/lazygit ]; alias lg="lazygit"; end
+	if [ -z "$TMUX" ]; tmux; end
 
 	eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 end

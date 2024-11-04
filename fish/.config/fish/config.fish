ac2ee7ec23c4fd2395043148e45657a38296c32b
Wesley Yuri <wesleyyuridev@gmail.com>
2024-11-03 23:09:30 -0300

    feat: separation of config file

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,21 +1,17 @@
 if status is-interactive
 	starship init fish | source
-	source ~/.asdf/asdf.fish
 	fish_add_path $HOME/.asdf/installs/php/8.3.12/.composer/vendor/bin
 	fish_add_path $HOME/.local/bin
 	. ~/.asdf/plugins/java/set-java-home.fish
-	source ~/.asdf/plugins/golang/set-env.fish
 
-	if [ -d $HOME/software-development ]; alias dev="cd $HOME/software-development"; end
-	if [ -e /bin/docker ]; alias docker-compose="docker compose"; end
-	if [ -e /bin/lsd ]; alias ls="lsd"; end
-	if [ -e /bin/nvim ]; alias vim="nvim"; end
-	if [ -e /bin/lazygit ]; alias lg="lazygit"; end
-	if [ -z "$TMUX" ]; tmux; end
+	source ~/.asdf/asdf.fish
+	source ~/.asdf/plugins/golang/set-env.fish
+	source ~/.config/fish/scripts/alias.fish
 
 	function reload
 		source $HOME/.config/fish/config.fish
 	end
-
-	eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+  
+  # Initialize session of tmux
+	if [ -z "$TMUX" ]; tmux; end
 end
---
+++ fish/.config/fish/scripts/alias.fish
@@ -0,0 +1,5 @@
+if [ -d $HOME/software-development ]; alias dev="cd $HOME/software-development/"; end
+if [ -e /bin/docker ]; alias docker-compose="docker compose"; end
+if [ -e /bin/lsd ]; alias ls="lsd"; end
+if [ -e /bin/nvim ]; alias vim="nvim"; end
+if [ -e /bin/lazygit ]; alias lg="lazygit"; end

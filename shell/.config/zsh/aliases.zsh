9be127bffeff3038428582bcb10845d709c905e2
Wesley Yuri <wesleyyuridev@gmail.com>
2025-09-25 23:25:43 -0300

    update zsh

--- shell/.config/zsh/aliases.zsh
+++ shell/.config/zsh/aliases.zsh
@@ -1,9 +1,13 @@
+# Sdk props
+export ANDROID_HOME=$HOME/Android/Sdk
+export PATH=$PATH:$ANDROID_HOME/emulator
+export PATH=$PATH:$ANDROID_HOME/platform-tools
+
 # env props
 export dotfiles_dir="$HOME/Dotfiles"
 export hypr_dir="$HOME/.config/hypr"
 export waybar_dir="$HOME/.config/waybar"
-
-export dev="$HOME/Development"
+export dev_dir="$HOME/Development"
 
 [[ -e /bin/lsd ]] && alias ls="lsd"
 
@@ -21,6 +25,6 @@ if [[ -d $dotfiles_dir ]]; then
   alias dotconfig="cd $dotfiles_dir && nvim $dotfiles_dir"
 fi
 
-if [[ -d $dev ]]; then
-  alias dev="cd $dev"
+if [[ -d $dev_dir ]]; then
+  alias dev="cd $dev_dir"
 fi
--- shell/.config/zsh/helpers.zsh
+++ shell/.config/zsh/helpers.zsh
@@ -2,3 +2,14 @@ reload ()
 {
   source $HOME/.zshrc
 }
+
+project ()
+{
+  project=$(fd --type d --max-depth 1 . $dev_dir | fzf)
+  cd "$project"
+}
+
+docker_create_mariabd ()
+{
+  docker run -d --name mariadb -e MYSQL_ROOT_PASSWORD=secret -p 3306:3306 -v $HOME/Development/masterix/bkp/:/bkp --restart unless-stopped mariadb
+}
